fe055c4d06dc27cbe033ab3b14bbeca4c4e2f888
NikolaiStrogov <NikolaiStrogov@gmail.com>
2025-07-12 01:17:37 +0700

    Вторая домашка по программированию и вёрстке

---
+++ hw-2.js
@@ -0,0 +1,79 @@
+//Задание 1
+let a;
+a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+//Задание 2
+let dateIphone = 2007;
+alert(dateIphone);
+
+//Задание 3
+let nameCreater = "Брендан Эйх";
+alert(nameCreater);
+
+//Задание 4
+let b = 10, c = 2;
+alert(b + c);
+
+//Задание 5
+let result;
+result =  2 ** 5;
+alert(result);
+
+//Задание 6
+a = 9;
+b = 2;
+result = a % 2;
+alert(result);
+
+//Задание 7
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+
+//Задание 8
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+//Задание 9
+let user = {
+    name: "Иван",
+    age: 21,
+    isAdmin: true
+}
+
+//Задание 10
+let nameUser = prompt("Ваше имя?");
+alert(`Привет, ${nameUser}`);
+
+
+//Дополнительное задание 1
+let number = prompt("Загадайте любое число!");
+
+//Дополнительное задание 2
+let doubleNumber = number * 2;
+alert(doubleNumber);
+
+//Дополнительное задание 3
+let addDoubleNumber = doubleNumber + 10;
+alert(addDoubleNumber);
+
+//Дополнительное задание 4
+let divideNumber = addDoubleNumber / 2;
+alert(divideNumber);
+
+//Дополнительное задание 5
+let finishResult = divideNumber - number;
+alert(finishResult);
+
+//Дополнительное задание 6
+alert("Ответ равен 5");
+
+
